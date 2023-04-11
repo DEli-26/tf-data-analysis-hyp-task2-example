664e9618b672c8d355e5699ecf219c3a46b98d16
Daniil <106452573+DEli-26@users.noreply.github.com>
2023-04-12 00:52:26 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,5 +11,11 @@ from scipy.stats import cramervonmises_2samp
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.03
     
-    p_value = cramervonmises_2samp(x=x, y=y, method='exact').pvalue
+    model = MMD(
+        compute_kernel='laplacian', bias='True'
+    )
+    _, p_value = model.test(x=x, 
+               y=y, 
+               random_state=26)
+    
     return p_value < alpha # Ваш ответ, True или False
